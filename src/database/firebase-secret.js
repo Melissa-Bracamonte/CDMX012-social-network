ae0457b01526911f59dcdcc0754982880816264b
Melissa Bracamonte <melissabracamonte30@gmail.com>
2022-04-13 14:38:09 -0500

    última actualización

---
+++ src/database/firebase-secret.js
@@ -0,0 +1,9 @@
+export const firebaseSecret = {
+  apiKey: 'AIzaSyAEn7GLyIgelQsOfKdz9bsXt0VNHYiN-bs',
+  authDomain: 'prueba1-fbaf1.firebaseapp.com',
+  projectId: 'prueba1-fbaf1',
+  storageBucket: 'prueba1-fbaf1.appspot.com',
+  messagingSenderId: '57763345832',
+  appId: '1:57763345832:web:bea038b2ab77f1b709199d',
+  measurementId: 'G-YSV74XZ7CT',
+};
